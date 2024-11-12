43936fc061b4ab841f39c8942a56695e74292fc3
Epic <byepic@outlook.es>
2024-11-12 20:41:13 -0300

    event list

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -8,3 +8,23 @@ btn1.value = "Caja qlia";
 btn1.type = "text";
 
 
+var span = document.createElement('span');
+let texto = document.createTextNode('Un lorem upso');
+span.appendChild(texto);
+document.body.appendChild(span);
+
+
+var caja = document.getElementById('box');
+var btn2 = document.createElement('input');
+btn2.setAttribute('type', 'button');
+btn2.setAttribute('value', 'Boton nuevo');
+document.body.replaceChild(btn2, caja);
+
+let box = document.getElementById('box');
+box.addEventListener('click', function(){ alert('Hola mundo'); });
+
+box.addEventListener('mouseover', function(){ alert('cambio'); });
+
+box.addEventListener('mouseout', function(){ alert('cambio denuevo'); });
+
+
