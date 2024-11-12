92f3e9677f1a99db3b2795337405d7e6bf3f0658
Epic <byepic@outlook.es>
2024-11-12 20:15:10 -0300

    una cajita de nada

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,2 +1,10 @@
 var parrafo = document.getElementById('parrafo');
 parrafo.innerHTML = 'Hola Mundo';
+parrafo.setAttribute('style', 'color: red;');
+
+var btn1 = document.getElementById('btn1');
+
+btn1.value = "Caja qlia";
+btn1.type = "text";
+
+
