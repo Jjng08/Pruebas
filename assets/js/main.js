5a0aa3b84c34ea52bbf8fe2e39d610c4d76e19bc
Epic <byepic@outlook.es>
2024-12-03 19:24:25 -0300

    attr de li

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,23 @@ $(document).ready(function() {
 //filtros
     $('ul.my-list>li>a:first').text('First');
     $('ul.my-list>li>a:first').attr('href', 'http://www.google.com');
-    $('ul.my-list>li>a:eq(4)').text('Last');
+    $('ul.my-list>li>a:eq(4)').text('a');
+   // $('ul.my-list>li>a:lt(5)').attr('target', '_blank');
+
+
+    $('input[type="checkbox"]:checked').fadeOut();
+
+    console.log($('ul.my-list>li').length);
+
+    var link = $('a[href]')[0];
+
+    alert(link.href);
+
+
+    $('ul.my-list').appendChild('<li></li>');
+    $('ul.my-list>li>a:last').text('last');
+    $('ul.my-list>li>a:last').attr('href','www.github.com')
+    $('ul.my-list:last').attr('target', '_blank');
+    
+    
 });
